da42c78d722baa7f5f4f4c59f1a961ae6cd565f8
Johan Martinsson <j.martinsson@bee-buzziness.com>
2019-01-01 21:49:33 +0100

    connect the whole thing to mongo instead of in-memory persistence

--- test/ApplicationSpec.ts
+++ test/ApplicationSpec.ts
@@ -1,20 +1,49 @@
-import { assert, expect } from 'chai';
-import express = require('express');
-import request = require('supertest-as-promised');
+import {expect} from 'chai';
+import {MongoClient} from 'mongodb'
+import express = require('express')
+import request = require('supertest-as-promised')
 
 function createApp() {
     let app = express()
-    app.get('/job-list/:contextId', (req, res) => {
-        res.send([])
+    app.get('/job-list/:contextId', async (req, res) => {
+        let jobList = await persistence.getEvents()
+        res.send(jobList)
     })
     return app
 }
 
-let persistence = {
-    addEvent(event) {
+class EventPersistence {
+    private readonly mongoUrl: string
+
+    constructor(mongoUrl: string) {
+        this.mongoUrl = mongoUrl
+    }
+
+    async addEvent(event: any) {
+        let collection = await this.getCollection()
+        await collection.insertOne(event)
+    }
+
+    async getEvents() {
+        let collection = await this.getCollection()
+        return collection.find().toArray()
 
     }
+
+    async deleteAllEvents() {
+        let collection = await this.getCollection()
+        return collection.deleteMany({})
+    }
+
+    private async getCollection() {
+        let client = await MongoClient.connect(this.mongoUrl)
+        return client.db('real-world-tdd').collection('rabbitEvents')
+    }
+
 }
+
+let persistence = new EventPersistence('mongodb://localhost')
+
 
 describe('Application', () => {
     describe('GET /job-list', () => {
@@ -25,6 +54,7 @@ describe('Application', () => {
         describe('returns all jobs corresponding to the given id, one entry per job', () => {
             it('no entries', async () => {
                 let app = createApp()
+                await persistence.deleteAllEvents()
 
                 let contextId = 'someId'
                 await request(app).get('/job-list/' + contextId)
@@ -33,12 +63,22 @@ describe('Application', () => {
                         expect(response.body).eql([])
                     })
             });
+
             // several entries
-            it('several entries', () => {
+            it('several entries', async () => {
                 let app = createApp();
 
-                persistence.addEvent({type:'start', data: {id:'1'}})
-                persistence.addEvent({type:'start', data: {id:'2'}})
+                await persistence.deleteAllEvents()
+                await persistence.addEvent({type: 'start', data: {id: '1'}})
+                await persistence.addEvent({type: 'start', data: {id: '2'}})
+
+                var contextId = "someId"
+                await request(app).get('/job-list/' + contextId)
+                    .expect(200)
+                    .expect(response => {
+                        expect(response.body).lengthOf(2)
+                    })
+
             });
 
         });
